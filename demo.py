537be330eecf4373aab8c3f21ac01e8922743547
leodd <410528518@qq.com>
2019-03-01 17:34:28 -0600

    add bethe approximation to the partition function

--- demo.py
+++ demo.py
@@ -8,10 +8,12 @@ A = np.array(
      [0, 1, 0]]
 )
 
-w = [0, 1]
-
-# ps = print_prob(A, w, 10, max_prod=True)
-# for p in ps:
-#     print(p)
+w = [1, 2]
 
 print(sumprod(A, w, 10))
+print(maxprod(A, w, 10))
+
+# you can even print the distribution of each variables
+ps = print_prob(A, w, 10, max_prod=False)
+for p in ps:
+    print(p)
